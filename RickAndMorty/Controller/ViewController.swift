b7957019258bd216e8cd24a09931c45fc7fa4847
Himik220 <1703_ptz@mail.ru>
2022-04-15 19:14:38 +0300

    Release №2

--- RickAndMorty/Controller/ViewController.swift
+++ RickAndMorty/Controller/ViewController.swift
@@ -11,19 +11,20 @@ import SDWebImage
 class ViewController: UIViewController {
     
     var identifier = "mainCell"
-    var results: Results?
+    var results: [Results] = []
     
     @IBOutlet weak var mainTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        NetworkDataFetcher.shared.fetchHeroes(urlString: "https://rickandmortyapi.com/api/character") {[weak self] (result:Results?) in
-            guard let result = result else {
-                return
+        for i in 1...42 {
+            NetworkDataFetcher.shared.fetchHeroes(urlString: "https://rickandmortyapi.com/api/character/?page=\(i)") {[weak self] (result:Results?) in
+                guard let result = result else {
+                    return
+                }
+                self?.results.append(result)
+                self?.mainTableView.reloadData()
             }
-            self?.results = result
-            self?.mainTableView.reloadData()
         }
     }
     
@@ -31,19 +32,34 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     //MARK: - UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return results.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results?.heroes.count ?? 0
+        switch section {
+        case 0...40:
+            return 20
+        case 41:
+            return 6
+        default:
+            break
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as?  CustomTableViewCell {
-            let hero = results?.heroes[indexPath.row]
-            cell.nameLabel.text = hero?.name
-            cell.genderLabel.text = hero?.gender
-            cell.speciesLabel.text = hero?.species
-            cell.avatarImageView.layer.cornerRadius = 15
-            cell.avatarImageView.sd_setImage(with: URL(string: hero?.image ?? ""))
-            return cell
+            if indexPath.section != 42 {
+                let hero = results[indexPath.section].heroes[indexPath.row]
+                cell.nameLabel.text = hero.name
+                cell.genderLabel.text = hero.gender
+                cell.speciesLabel.text = hero.species
+                cell.avatarImageView.layer.cornerRadius = 15
+                cell.avatarImageView.sd_setImage(with: URL(string: hero.image))
+                return cell
+            } 
         }
         return UITableViewCell()
     }
@@ -56,7 +72,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let detailVC = storyboard.instantiateViewController(identifier: "detailVC") as? DetailViewController {
-            detailVC.idHero = results!.heroes[indexPath.row].id
+            detailVC.idHero = results[indexPath.section].heroes[indexPath.row].id
             showDetailViewController(detailVC, sender: nil)
         }
     }
